238bfb37777c9bc906f325789e30f02c0a46f809
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-04-19 15:42:48 +0530

    Add : Django User cusomized accounts app :hammer:

---
+++ accounts/admin.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+- accounts.admin
+~~~~~~~~~~~~~~
+
+- This file contains the admin models of accounts.
+"""
+
+# future
+from __future__ import unicode_literals
+
+# 3rd party
+
+
+# Django
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+# local
+
+# own app
+from accounts.forms import UserCreationForm, AdminUserChangeForm
+
+
+User = get_user_model()
+
+USERNAME_FIELD = User.USERNAME_FIELD
+
+REQUIRED_FIELDS = (USERNAME_FIELD,) + tuple(User.REQUIRED_FIELDS)
+
+BASE_FIELDS = (None, {
+    'fields': REQUIRED_FIELDS + ('password',),
+})
+
+PROFILE_FIELDS = (_('Profile'), {
+    'fields': ('first_name', 'last_name', 'uuid', 'avatar'),
+})
+
+PERMISSION_FIELDS = (_('Permissions'), {
+    'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',),
+})
+
+DATE_FIELDS = (_('Important dates'), {
+    'fields': ('last_login', 'date_joined',),
+})
+
+
+class UserAdmin(DjangoUserAdmin):
+    """
+
+    """
+    add_form_template = None
+    add_form = UserCreationForm
+    form = AdminUserChangeForm
+    list_per_page = 1000
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = (USERNAME_FIELD, 'first_name', 'last_name', 'uuid', 'avatar_thumbnail', 'is_active', )
+    list_display_links = (USERNAME_FIELD, 'uuid',)
+    search_fields = ('uuid', USERNAME_FIELD, 'first_name', 'last_name',)
+    fieldsets = (
+        BASE_FIELDS,
+        PROFILE_FIELDS,
+        PERMISSION_FIELDS,
+        DATE_FIELDS,
+    )
+    list_filter = ('is_superuser', 'is_staff', 'is_active',)
+    add_fieldsets = (
+        (None, {
+            'fields': REQUIRED_FIELDS + (
+                'password1',
+                'password2',
+            ),
+        }),
+    )
+    ordering = None
+    filter_horizontal = ('groups', 'user_permissions',)
+    readonly_fields = ('uuid', 'last_login', 'date_joined')
+
+    def avatar_thumbnail(self, obj):
+        """
+
+        :param obj: user instance
+        :return: user avatar thumbanil
+        """
+        return '<img width="36" height="36" src="%s"/>' % obj.avatar_thumbnail.url if bool(obj.avatar_thumbnail) else ''
+
+    avatar_thumbnail.allow_tags = True
+
+admin.site.register(User, UserAdmin)
---
+++ accounts/models.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+- accounts.models
+~~~~~~~~~~~~~~
+
+- This file contains the Accounts(user) models that will map into DB tables.
+
+"""
+
+# future
+from __future__ import unicode_literals
+
+# 3rd party
+import os, uuid
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+# Django
+from django.db import models
+from django.core import validators
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+# local
+
+# own app
+
+
+def upload_avatar_to(instance, filename):
+    """
+    Get unique filename for user's profile picture.
+    """
+    filename_base, filename_ext = os.path.splitext(filename)
+    return 'profile_picture/{0}.{1}'.format(
+        instance.uuid,
+        filename_ext.lower(),
+    )
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom manager for django's custom User Model.
+    Support custom fields (fields other than django.contrib.auth.models.AbstractBaseUser) update while creating user.
+    """
+    def create_user(self, password=None, **kwargs):
+        user = self.model(**kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, **kwargs):
+        user = self.create_user(**kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Store User account details.
+    Support custom fields (fields other than django.contrib.auth.models.AbstractBaseUser) to be stored in db.
+    """
+    first_name = models.CharField(
+        _('first name'),
+        max_length=30,
+        null=True,
+        blank=True)
+    last_name = models.CharField(
+        _('last name'),
+        max_length=30,
+        null=True,
+        blank=True)
+    username = models.CharField(
+        _('username'),
+        max_length=75,
+        unique=True,
+        help_text=_('Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[
+            validators.RegexValidator(
+                r'^[\w.@+-]+$',
+                _('Enter a valid username. This value may contain only '
+                  'letters, numbers ' 'and @/./+/-/_ characters.')
+            ),
+        ],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    uuid = models.UUIDField(
+        _('User Unique Identifier'),
+        default=uuid.uuid4,
+        help_text=_('User uuid, this token will be used to link user in other services.'),
+    )
+
+    avatar = models.ImageField(
+        _('User Profile Picture'),
+        upload_to=upload_avatar_to
+    )
+    avatar_thumbnail = ImageSpecField(
+        source='avatar',
+        processors=[ResizeToFill(100, 50)],
+        format='JPEG',
+        options={'quality': 60}
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.')
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active.  Unselect this instead of deleting accounts.')
+    )
+    date_joined = models.DateTimeField(
+        _('date joined'),
+        default=timezone.now
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
+        db_table = 'auth_user'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        ordering = ['username', ]
+
+    def __str__(self):
+        return '<{uuid}>: {username}'.format(
+            uuid=self.uuid,
+            username=self.username,
+        )
+
+    def get_full_name(self):
+        """Return full name of user"""
+        return '{0} {1}'.format(self.first_name, self.last_name)
+
+    def get_short_name(self):
+        """
+        Return short name of user.
+        """
+        return '{0}'.format(self.first_name)
