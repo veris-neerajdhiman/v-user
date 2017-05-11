41994b1ccecac1badcdbc39ed761d8f68043cbe4
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-05-11 16:00:34 +0530

    test : settings test cases added :white_check_mark:

---
+++ accounts/tests/test_settings.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+- accounts..tests.test_settings
+~~~~~~~~~~~~~~
+
+- This file make sure all necessary settings are their for user micro-service
+
+"""
+
+# future
+from __future__ import unicode_literals
+
+# Django
+from django.conf import settings
+from django.test import TestCase
+
+
+class SettingsTestCase(TestCase):
+    """Settings Test Case
+
+    """
+
+    def setUp(self):
+        """
+
+        """
+        self.env_settings = ['AM_SERVER_URL', ]
+
+    def test_environment_variables(self):
+        """makes sure all env variables which are necessary for running User micro-service are defined in settings
+
+        """
+
+        for env_setting in self.env_settings:
+            if not getattr(settings, env_setting):
+                self.assertFalse('{0} environment settings is defined.'.format(env_setting))
+
+        self.assertTrue('Environment settings test passed.')
+
+
